8d6139ecdf53a0c0fad7991f379bb64b61e8fd53
sama3612 <sama3612@colorado.edu>
2021-02-02 17:15:17 -0700

    pair 2 added to ship file

--- src/edu/colorado/dreamteam/Ship.java
+++ src/edu/colorado/dreamteam/Ship.java
@@ -6,6 +6,7 @@ public class Ship {
     // TODO: create appropriate getter and setter methods
     // TODO: Understand encapsulation
     // TODO: Understand what these todo comments mean
+    //Team DreamTeam, pair 2 was here
 
     public  void show()     {     // dunno why this is here maybe it is just an example method
         System.out.println("IF you can't see this then something is severely wrong!!");

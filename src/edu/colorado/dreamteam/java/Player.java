a1cdaff0ad6ac32c10e96bee4eaad69299f38185
sama3612 <sama3612@colorado.edu>
2021-02-14 16:00:43 -0700

    Created PlayerTest, updated Player class accordingly

--- src/edu/colorado/dreamteam/java/Player.java
+++ src/edu/colorado/dreamteam/java/Player.java
@@ -2,11 +2,22 @@ package edu.colorado.dreamteam.java;
 
 public class Player {
     private String name;
-    public Player(String name) {
+    private Map map;
+
+    public Player(String name, Map map) {
         this.name = name;
+        this.map = map;
     }
 
     public String getName() {
         return name;
+    }
+
+    public boolean getAttacked(Coordinate attack) {
+        return map.getAttacked(attack);
+    }
+
+    public boolean isAlive() {
+        return map.hasShips();
     }
 }
---
+++ src/edu/colorado/dreamteam/test/PlayerTest.java
@@ -0,0 +1,32 @@
+package edu.colorado.dreamteam.test;
+
+import edu.colorado.dreamteam.java.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+    @Test
+    public void playerCreated() {
+        Player player = new Player("test", new Map());
+    }
+
+    @Test
+    public void playerCreated() {
+        Player player = new Player("test", new Map());
+        assertThat(player.getName(), is("test"));
+    }
+
+    @Test
+    public void playerIsAlive() {
+        //create player with map, attack remaining ship, check if isAlive is set to false
+    }
+
+    @Test
+    public void playerGotAttacked() {
+        //create player with map, attack fleet, check if map and fields are updated properly
+    }
+}

7f36c570e3bd34695604dcfdd08c9a5e4ee1410d
sama3612 <sama3612@colorado.edu>
2021-02-14 16:01:57 -0700

    Added some TODO comments and starter code for Map and UI class so that we can hand off code for the next pair

--- src/edu/colorado/dreamteam/java/Map.java
+++ src/edu/colorado/dreamteam/java/Map.java
@@ -1,4 +1,16 @@
 package edu.colorado.dreamteam.java;
 
+// TODO: initialize map (maybe a constructor too). Add necessary private variables to keep track of state of map for each player
+// TODO: implement hasShips. Check if the map has any ships left
+// TODO: implement getAttacked. takes in an attack Coordinate and attacks the ship at that location. returns whether it was a successful attack
+// TODO: check if the spot was already attacked (if it was, "tuff luck, better luck next time" and its the other persons turn)
+// TODO: add other methods that may be useful (how many ships left,
 public class Map {
+    public boolean hasShips() {
+        return false;
+    }
+
+    public boolean getAttacked(Coordinate attack) {
+        return false;
+    }
 }
--- src/edu/colorado/dreamteam/java/UI.java
+++ src/edu/colorado/dreamteam/java/UI.java
@@ -1,6 +1,10 @@
 package edu.colorado.dreamteam.java;
 import java.util.Scanner;
 
+// UI class is used by both players, so keep all methods static
+// TODO: implement getAttack that takes in user input
+// TODO: implement displayMap that displays your own map first (first parameter) and then the opposing map as well (2nd parameter)
+// TODO: initialize map for each player based on user input
 public class UI {
     public static Player initPlayer() {
         Scanner sc = new Scanner(System.in);
@@ -9,6 +13,15 @@ public class UI {
         String name = sc.nextLine();
         System.out.println("Awesome, good luck " + name);
 
-        return new Player(name);
+        //ADD CODE HERE TO INITIALIZE A MAP FOR PLAYER AND RETURN PLAYER WITH THEIR MAP
+
+        return new Player(name, map);
+    }
+
+    public static Coordinate getAttack(Player player) {
+        return null;
+    }
+
+    public static void displayMap(Player player2, Player player1) {
     }
 }
